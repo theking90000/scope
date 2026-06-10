f6b29f1cd69aead3e647ad3605066fe82165c649
theking90000 <martin.cogh@gmail.com>
2026-06-10 08:31:30 +0200

    Update JUnit BOM version to 6.1.0 in build.gradle.kts

--- scope/build.gradle.kts
+++ scope/build.gradle.kts
@@ -1,5 +1,5 @@
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.13.4"))
+    testImplementation(platform("org.junit:junit-bom:6.1.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
